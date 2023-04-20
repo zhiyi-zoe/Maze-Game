87b08c1188dd69eb36e502174a0be84e7617ab17
Zhiyi Tang <zoe01@berkeley.edu>
2023-04-19 20:59:17 -0700

    phase2 tang2

--- proj3/byow/Core/HeadsUpDisplay.java
+++ proj3/byow/Core/HeadsUpDisplay.java
@@ -9,10 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HeadsUpDisplay {
+    public int width;
+    public int height;
     public TETile[][] world;
     public int[] avatar;
     public List<TETile> appearanceList;
     public HeadsUpDisplay(TETile[][] world, int[] avatar) {
+        this.width = world.length;
+        this.height = world[0].length;
         this.world = world;
         this.avatar = avatar;
         this.appearanceList = creatAppearanceList();
@@ -25,7 +29,7 @@ public class HeadsUpDisplay {
         TETile current = world[mouseX][mouseY];
         String description = current.description();
         return description;
-        //int numYTiles = world[0].length;
+        //int numYTiles = height;
         //StdDraw.text(0, numYTiles, description);
         //renderFrame(world);
         //StdDraw.pause(10);
@@ -54,5 +58,18 @@ public class HeadsUpDisplay {
             nextAppearance = appearanceList.get(currentAppearanceIndex + 1);
         }
         world[currentX][currentY] = nextAppearance;
+    }
+    public void creatMenu() {
+        StdDraw.clear(Color.BLACK);
+        StdDraw.setPenColor(Color.WHITE);
+        Font fontSmall = new Font("Monaco", Font.BOLD, 10);
+        StdDraw.setFont(fontSmall);
+        int textGap = this.height / 2 / 2;
+        StdDraw.text(this.width / 2, this.height / 2 + textGap, "Welcome to BYOW");
+        StdDraw.text(this.width / 2, this.height / 2, "New Game (N)");
+        StdDraw.text(this.width / 2, this.height / 2 - textGap / 2 / 2, "Load Game (L)");
+        StdDraw.text(this.width / 2, this.height / 2 - textGap / 2, "Replay Game (R)");
+        StdDraw.text(this.width/ 2, this.height / 2 - textGap / 2 / 2 - textGap / 2,
+                "Quit (Q)");
     }
 }
